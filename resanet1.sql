4871da88980695ec6454fde43e10dc8f99fe2cb4
WILMANNflorian <Florian.Wilmann.Sio@gmail.com>
2018-05-16 14:09:24 +0200

    Requete SQL

---
+++ resanet1.sql
@@ -0,0 +1,8 @@
+insert into JoursFeries values(1,1,YEAR(current_date()), 'Nouvel an') ;
+insert into JoursFeries values(1,5,YEAR(current_date()), 'Fête du travail') ;
+insert into JoursFeries values(8,5,YEAR(current_date()), '8 mai 1945') ;
+insert into JoursFeries values(14,7,YEAR(current_date()), 'Fête Nationale') ;
+insert into JoursFeries values(15,8,YEAR(current_date()), 'Asomption') ;
+insert into JoursFeries values(1,11,YEAR(current_date()), 'La Toussaint') ;
+insert into JoursFeries values(11,11,YEAR(current_date()), 'Armistice') ;
+insert into JoursFeries values(25,12,YEAR(current_date()), 'Noël') ;
